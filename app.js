a51dc01894af5231156244afcb2224152634f89a
ChandrahassTvs <chandrahass1937@gmail.com>
2017-05-10 18:52:25 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
- app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'hello') {
-      res.send(req.query['hub.challenge']);
-    } else {
-      res.send('Error, wrong validation token');    
-    }
-  });
