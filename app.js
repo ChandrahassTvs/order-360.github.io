9c32fb70befee7bad6cded07d71a43aab75916a5
ChandrahassTvs <chandrahass1937@gmail.com>
2017-05-10 18:47:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === "hello") {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
+ app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'hello') {
+      res.send(req.query['hub.challenge']);
+    } else {
+      res.send('Error, wrong validation token');    
+    }
+  });
